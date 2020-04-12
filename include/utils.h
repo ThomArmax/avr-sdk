b547945739667aac4ec438f1dd0c4b265d6a8486
Thomas COIN <esvcorp@gmail.com>
2020-04-12 18:44:35 +0200

    [Doc] Document bit macros

--- include/utils.h
+++ include/utils.h
@@ -11,9 +11,33 @@
  * @{
  */
 
+/**
+ * @def bit_set
+ * @brief Set a bit to 1L
+ * @param byte
+ * @param nbit
+ */
 #define bit_set(byte,nbit)   ((byte) |=  (1<<(nbit)))
+/**
+ * @def bit_clear
+ * @brief Set a bit to 0L
+ * @param byte
+ * @param nbit
+ */
 #define bit_clear(byte,nbit) ((byte) &= ~(1<<(nbit)))
+/**
+ * @def bit_flip
+ * @brief Toggle/Invert a bit
+ * @param byte
+ * @param nbit
+ */
 #define bit_flip(byte,nbit)  ((byte) ^=  (1<<(nbit)))
+/**
+ * @def bit_check
+ * @brief Check whether or not a bit is set
+ * @param byte
+ * @param nbit
+ */
 #define bit_check(byte,nbit) ((byte) &   (1<<(nbit)))
 
 /**
