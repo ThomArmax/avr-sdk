6501eeff6222db0e006ebee923dbd9bf13d25f1d
Thomas COIN <esvcorp@gmail.com>
2020-04-13 18:08:24 +0200

    Improve bit utils

--- include/utils.h
+++ include/utils.h
@@ -12,33 +12,70 @@
  */
 
 /**
- * @def bit_set
- * @brief Set a bit to 1L
+ * @brief Set a bit to `1L`
  * @param byte
  * @param nbit
  */
-#define bit_set(byte,nbit)   ((byte) |=  (1<<(nbit)))
+#define BIT_SET(byte,nbit) ((byte) |= (1<<(nbit)))
 /**
- * @def bit_clear
- * @brief Set a bit to 0L
+ * @brief Set a bit to `0L`
  * @param byte
  * @param nbit
  */
-#define bit_clear(byte,nbit) ((byte) &= ~(1<<(nbit)))
+#define BIT_CLEAR(byte,nbit) ((byte) &= ~(1<<(nbit)))
 /**
- * @def bit_flip
- * @brief Toggle/Invert a bit
+ * @brief Toggle a bit
  * @param byte
  * @param nbit
  */
-#define bit_flip(byte,nbit)  ((byte) ^=  (1<<(nbit)))
+#define BIT_TOGGLE(byte,nbit) ((byte) ^= (1<<(nbit)))
 /**
- * @def bit_check
  * @brief Check whether or not a bit is set
  * @param byte
  * @param nbit
  */
-#define bit_check(byte,nbit) ((byte) &   (1<<(nbit)))
+#define BIT_CHECK(byte,nbit) ((byte) & (1<<(nbit)))
+
+/**
+ * @brief Set a bit to `1L`
+ * @param reg
+ * @param nbit
+ */
+static inline void bit_set(volatile uint8_t* reg, uint8_t nbit)
+{
+    *reg |= (1 << nbit);
+}
+
+/**
+ * @brief Set a bit to `0L`
+ * @param reg
+ * @param nbit
+ */
+static inline void bit_clear(volatile uint8_t* reg, uint8_t nbit)
+{
+    *reg &= ~(1 << nbit);
+}
+
+/**
+ * @brief Toggle a bit
+ * @param reg
+ * @param nbit
+ */
+static inline void bit_toggle(volatile uint8_t* reg, uint8_t nbit)
+{
+    *reg ^= (1 << nbit);
+}
+
+/**
+ * @brief Check whether or not a bit is set
+ * @param byte
+ * @param nbit
+ * @return `1` if bit is set; `0` otherwise
+ */
+static inline uint8_t bit_check(uint8_t reg, uint8_t nbit)
+{
+    return (reg & (1 << nbit));
+}
 
 /**
  * @}
